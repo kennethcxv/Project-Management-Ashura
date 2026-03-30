7326b443b431a1de0d6e15ac69083a431898d194
Kenneth Camacho <Kennethcxv@gmail.com>
2026-03-29 23:05:25 -0400

    feat(tasks): add task form UI, comments, and close button

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,18 +1,75 @@
 import Dialog from "@mui/material/Dialog"
-import DialogTitle from "@mui/material/DialogTitle"
 import TextField from "@mui/material/TextField"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import DialogTitle from "@mui/material/DialogTitle"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import { setTitleAction,closeAction, setDescriptionAction,setSubTaskAction,CreateTaskAction,setCommentAction,CreateCommentAction  } from "../redux/slices/createTask"
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+
 
 const CreateTask = () => {
 
     const open = useSelector((state) => state.CreateTask.open)
-    
+    const title = useSelector((state) => state.CreateTask.title)
+    const description = useSelector((state) => state.CreateTask.description)
+    const subTask = useSelector((state) => state.CreateTask.subTask)
+    const AddNewTaskArr = useSelector((state) => state.CreateTask.AddNewTaskArr)
+    const comment = useSelector((state) => state.CreateTask.comment)
+    const commentArray = useSelector((state) => state.CreateTask.commentArray)
+
+    const dispatch = useDispatch()
+
+    const handleCreateTask = () => {
+        dispatch(CreateTaskAction())
+    }
+
+    const handleCreateComment = () => {
+        dispatch(CreateCommentAction())
+    }
+
+    const handleCloseAction = () => {
+        dispatch(closeAction())
+    }
+
     return (
     <>
         <Dialog open={open}>
-            <DialogTitle>Add Title</DialogTitle>
-            <TextField label="Title"/>
+            <Box sx={{display:"flex", justifyContent: "space-between", alignItems: "center", cursor:"pointer"}}>
+            <DialogTitle >Add a task</DialogTitle>
+            <CloseOutlinedIcon sx={{mr:2}}  onClick={handleCloseAction} />
+            </Box>
+
+            <Box sx={{display:"flex", flexDirection:"column", alignItems:"center", gap:2, p:2}}>
+                <TextField sx={{width:"500px",  justifyContent:"center"}}  placeholder="Add a title" value={title} onChange={(e) => dispatch(setTitleAction(e.target.value))} />
+                <TextField sx={{width:"500px"}} placeholder="Add a Description" value={description} onChange={(e) => dispatch(setDescriptionAction(e.target.value))} />
+                <TextField sx={{width:"500px"}} placeholder="Add a SubTask" value={subTask} onChange={(e) => dispatch(setSubTaskAction(e.target.value)) } />
+                <Button  onClick={() => handleCreateTask()} variant="contained">Create Task</Button>         
+
+                <DialogTitle>Activity</DialogTitle>
+
+                <TextField sx={{width:"500px"}} placeholder="Add a Comment" value={comment} onChange={(e) => dispatch(setCommentAction(e.target.value))} />       
+                <Button variant="contained"  onClick={() => handleCreateComment()}>Comment</Button>
+                {
+                    commentArray.map((item) => {
+                        return(
+                            <>
+                                <p>{item}</p>
+                            </>
+                        )
+                    }) 
+                }
+            </Box>
+    
         </Dialog>
+        {
+            AddNewTaskArr.map((item) => {
+                return(
+                    <p>{item.title}{item.description}{item.subTask}</p>
+                )
+            })
+        }
+        
     </>
   )
 }
--- src/redux/slices/createTask.ts
+++ src/redux/slices/createTask.ts
@@ -2,8 +2,16 @@ import { createSlice } from '@reduxjs/toolkit'
 
 // We need to make it so that when the user clicks on the button a dialog box opens. 
 
+// We need to show it on screen we do this by on button click it goes to a new array
+
 const initialState = {
-    open:false
+    open:false,
+    title:"",
+    description:"",
+    subTask:"",
+    AddNewTaskArr:[],
+    comment:"",
+    commentArray:[],
 }
 
 const createTask = createSlice({
@@ -17,10 +25,35 @@ const createTask = createSlice({
     closeAction:(state) => {
         state.open = false
         console.log(state.open)
-    }
+    },
+    setTitleAction:(state,action) => {
+      state.title = action.payload
+    },
+    setDescriptionAction:(state,action) => {
+      state.description = action.payload
+    },
+    setSubTaskAction:(state,action) => {
+      state.subTask = action.payload
+    },
+    CreateTaskAction:(state) => {
+      const newTask = {
+        title:state.title,
+        description:state.description,
+        subTask:state.subTask
+      }
+      state.AddNewTaskArr.push(newTask)
+      console.log(newTask)
+    },
+    setCommentAction:(state,action) => {
+      state.comment = action.payload
+    },
+    CreateCommentAction:(state) => {
+      console.log("createCommentAction")
+      state.commentArray.push(state.comment)
+    },
   }
 });
 
-export const {openAction,closeAction} = createTask.actions
+export const {openAction,closeAction,CreateTaskAction,setTitleAction,setDescriptionAction,setSubTaskAction,setCommentAction,CreateCommentAction} = createTask.actions
 
 export default createTask.reducer
